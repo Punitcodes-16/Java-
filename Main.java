76a935a0d806247500835aed5cef47cfeba2b04e
Punit <sharmapunit1622006@gmail.com>
2025-05-29 21:49:04 +0530

    Added first java practice files

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
