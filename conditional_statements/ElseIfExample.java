4ba905866bbf3e35683765a3700cf37dbdd1fa5e
Punit <sharmapunit1622006@gmail.com>
2025-05-31 20:59:26 +0530

    adding conditional statement example

---
+++ conditional_statements/ElseIfExample.java
@@ -0,0 +1,15 @@
+package conditional_statements;
+
+public class ElseIfExample {
+    public static void main(String[] args) {
+        int number = 15;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
