40ef3a9c024625038857eb4f2ad9aab0447a6b2a
Punit <sharmapunit1622006@gmail.com>
2025-06-29 14:04:17 +0530

    Java codes

---
+++ Arrays/PrintingArraywithLoop.java
@@ -0,0 +1,12 @@
+public class PrintingArraywithLoop {
+    public static void main(String[] args) {
+         int[] marks = new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
