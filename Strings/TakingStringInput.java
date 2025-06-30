99c60d0a54ce3149401d5f0290468f736b2236ad
Punit <sharmapunit1622006@gmail.com>
2025-06-30 19:25:21 +0530

    java codes

---
+++ Strings/TakingStringInput.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class TakingStringInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("your name is :"+ name);
+    }
+}
