da957c56429010f3c0c0c89474621425b8892016
Punit <sharmapunit1622006@gmail.com>
2025-06-30 20:47:47 +0530

    Java codes

---
+++ Strings/Substring.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Substring {
+    public static void main(String[] args) {
+        //compare
+        String sentence = "DeathCruger";
+        String name = sentence.substring(5);
+        System.out.println(name);
+
+        
+    }
+
+}
